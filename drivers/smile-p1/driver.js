37fe6cd777c0ed862608de7da301c80d5a5afabd
Emile Nijssen <emile@emilenijssen.nl>
2021-04-15 15:47:28 +0200

    fix P1 meter firmware > 3

--- drivers/smile-p1/driver.js
+++ drivers/smile-p1/driver.js
@@ -10,7 +10,8 @@ module.exports = class PlugwiseSmileP1Driver extends PlugwiseDriver {
   }
   
   onPairFilterAppliance({ appliance, bridge }) {
-    if( !bridge.version.startsWith('3.') ) return false;
+    const bridgeVersionMajor = parseInt(bridge.version.split('.'));
+    if( bridgeVersionMajor < 3 ) return false;
     appliance.name = 'Smile P1';
     return appliance.type === 'gateway';
   }

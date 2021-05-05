d6e6aa23a86a64d86ada83344a022610cfeb7382
Emile Nijssen <emile@emilenijssen.nl>
2021-05-05 20:23:43 +0200

    add P1 meter_gas, meter_power.produced

--- drivers/smile-p1/device.js
+++ drivers/smile-p1/device.js
@@ -4,6 +4,26 @@ const PlugwiseDevice = require('../../lib/PlugwiseDevice');
 
 module.exports = class PlugwiseSmileP1Device extends PlugwiseDevice {
 
+  onInit(...props) {
+    super.onInit(...props);
+
+    if (!this.hasCapability('meter_gas')) {
+      this.addCapability('meter_gas').catch(this.error);
+    }
+
+    if (!this.hasCapability('meter_power')) {
+      this.addCapability('meter_power').catch(this.error);
+    }
+
+    if (!this.hasCapability('meter_power.produced')) {
+      this.addCapability('meter_power.produced').catch(this.error);
+    }
+
+    if (!this.hasCapability('meter_gas')) {
+      this.addCapability('meter_gas').catch(this.error);
+    }
+  }
+
   onPoll({ payload }) {
     // console.log(JSON.stringify(payload, false, 2));
     super.onPoll({ payload });
@@ -13,6 +33,7 @@ module.exports = class PlugwiseSmileP1Device extends PlugwiseDevice {
     const { logs } = location;
     if (!logs) return;
 
+    // measure_power
     if (Array.isArray(logs.point_log)) {
       const log = logs.point_log.filter(log => {
         if (log.type === 'electricity_consumed'
@@ -23,13 +44,36 @@ module.exports = class PlugwiseSmileP1Device extends PlugwiseDevice {
       }).pop();
 
       if (log) {
-        const value = log.period.measurement.reduce((total, item) => {
-          return total + parseFloat(item.$text);
-        }, 0);
+        const value = Array.isArray(log.period.measurement)
+          ? log.period.measurement.reduce((total, item) => {
+            return total + parseFloat(item.$text);
+          }, 0)
+          : parseFloat(log.period.measurement.$text);
         this.setCapabilityValue('measure_power', value).catch(this.error);
       }
     }
 
+    // meter_gas
+    if (Array.isArray(logs.cumulative_log)) {
+      const log = logs.cumulative_log.filter(log => {
+        if (log.type === 'gas_consumed'
+          && log.unit === 'm3'
+          && log.period
+          && log.period.measurement) return true;
+        return false;
+      }).pop();
+
+      if (log) {
+        const value = Array.isArray(log.period.measurement)
+          ? log.period.measurement.reduce((total, item) => {
+            return total + parseFloat(item.$text);
+          }, 0)
+          : parseFloat(log.period.measurement.$text);
+        this.setCapabilityValue('meter_gas', value).catch(this.error);
+      }
+    }
+
+    // meter_power
     if (Array.isArray(logs.cumulative_log)) {
       const log = logs.cumulative_log.filter(log => {
         if (log.type === 'electricity_consumed'
@@ -40,16 +84,34 @@ module.exports = class PlugwiseSmileP1Device extends PlugwiseDevice {
       }).pop();
 
       if (log) {
-        const value = log.period.measurement.reduce((total, item) => {
-          return total + parseFloat(item.$text) / 1000;
-        }, 0);
+        const value = Array.isArray(log.period.measurement)
+          ? log.period.measurement.reduce((total, item) => {
+            return total + parseFloat(item.$text) / 1000;
+          }, 0)
+          : parseFloat(log.period.measurement.$text);
         this.setCapabilityValue('meter_power', value).catch(this.error);
       }
     }
 
-    // TODO: Add Electricity Produced
+    // meter_power.produced
+    if (Array.isArray(logs.cumulative_log)) {
+      const log = logs.cumulative_log.filter(log => {
+        if (log.type === 'electricity_produced'
+          && log.unit === 'W'
+          && log.period
+          && log.period.measurement) return true;
+        return false;
+      }).pop();
 
-    // TODO: Add Gas
+      if (log) {
+        const value = Array.isArray(log.period.measurement)
+          ? log.period.measurement.reduce((total, item) => {
+            return total + parseFloat(item.$text);
+          }, 0)
+          : parseFloat(log.period.measurement.$text);
+        this.setCapabilityValue('meter_power.produced', value).catch(this.error);
+      }
+    }
   }
 
 };

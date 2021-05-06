3384e9e6d6b09a75de7577667d8f1739c478e685
Emile Nijssen <emile@emilenijssen.nl>
2021-05-06 14:03:04 +0200

    sync Anna's measure_temperature & target_temperature

--- drivers/anna/device.js
+++ drivers/anna/device.js
@@ -37,11 +37,46 @@ module.exports = class PlugwiseAnnaDevice extends PlugwiseThermostatDevice {
         return location.$attr.id === this.locationId;
       });
 
+      // location_preset
       if (location
        && location.preset) {
         this.setCapabilityValue('location_preset', location.preset || null).catch(this.error);
       }
+
+      // target_temperature
+      if (location
+       && location.actuator_functionalities
+       && location.actuator_functionalities.thermostat_functionality) {
+        const value = parseFloat(location.actuator_functionalities.thermostat_functionality.setpoint);
+        this.setCapabilityValue('target_temperature', value).catch(this.error);
+      }
     }
+
+    // measure_temperature
+    if (appliance
+     && appliance.logs
+     && Array.isArray(appliance.logs.point_log)) {
+      appliance.logs.point_log.forEach(log => {
+        if (log.type === 'temperature'
+            && log.unit === 'C'
+            && log.period
+            && log.period.measurement) {
+          const value = parseFloat(log.period.measurement.$text);
+          this.setCapabilityValue('measure_temperature', value).catch(this.error);
+        }
+      });
+    }
+
+    // target_temperature
+
+    // if (payload
+    //     && payload.location.actuator_functionalities
+    //     && payload.location.actuator_functionalities.thermostat_functionality) {
+    //   const { setpoint } = payload.location.actuator_functionalities.thermostat_functionality;
+    //   const value = parseFloat(setpoint);
+    //   console.log('t', { value });
+    //   this.setCapabilityValue('target_temperature', value).catch(this.error);
+    // }
 
     // measure_luminance
     if (appliance
